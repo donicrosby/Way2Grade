e46ea269f3dc7317837ef090cd66dd01fa10a7e1
jeansburger <doni.crosby1995@gmail.com>
2016-09-11 21:57:25 -0400

    First File

---
+++ Way2Grade.py
@@ -0,0 +1,8 @@
+# Way2Grade, released under GPLv3
+
+"""*******************************************
+    Purpose:
+        This is the main function
+*******************************************"""
+
+def Way2Grade():
